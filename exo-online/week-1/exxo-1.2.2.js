aea94eaeb5dd1c73b0d373ee6b44e7a6079807d7
Rynthak <mehdi.boutoubat@gmail.com>
2019-01-16 17:23:01 +0100

    add minor fixes for factorization

--- exo-online/week-1/exxo-1.2.2.js
+++ exo-online/week-1/exxo-1.2.2.js
@@ -5,6 +5,11 @@ let aTab	=	[];
 //Initialisation taille max du bloc
 let maxSize	=	6000;
 
+//Initialisation variable maximum de tips dans l'ensemble des combinaisons générées
+ 
+let transacCombinaison = {maxTips: 0, table:""};
+
+console.log(transacCombinaison);
 aTab.push({"size": 2000,"tips": 13000});
 aTab.push({"size": 6000,"tips": 9000});
 aTab.push({"size": 800,"tips": 2000});
@@ -15,7 +20,7 @@ aTab.push({"size": 1300,"tips": 5000});
 aTab.push({"size": 600,"tips": 1500});
 
 //Calcul du combinatoire de tableau avec prise en compte du nom dépassement de la taille du bloc
-var res = [];
+ 
 for (let i = 0; i < Math.pow(2, aTab.length); i++) {
 	let bin = (i).toString(2), set = [];
     bin = new Array((aTab.length-bin.length)+1).join("0")+bin;
@@ -29,24 +34,14 @@ for (let i = 0; i < Math.pow(2, aTab.length); i++) {
         }
     }
     //On affecte la combinaison si la somme des bloc des transactions est inférieur à la somme du bloc total
-    if(set.blocksize<=maxSize){
-    	res.push(set);
+    if(set.blocksize<=maxSize && transacCombinaison.maxTips<=set.tips){
+    	transacCombinaison.table=set;
+    	transacCombinaison.maxTips=set.tips;
     }
 }
-
-//On cherche le maximum de tips dans l'ensemble des combinaisons générées
-let maxTips=0;
-let transacCombinaison=null;
-
-for(let i =0;i < res.length;i++){
-	if(res[i].tips >= maxTips){
-		maxTips=res[i].tips;
-		transacCombinaison=res[i];
-	}
-}
-console.log("Nombre max de tips: "+maxTips);
+console.log("Nombre max de tips: "+transacCombinaison.maxTips);
 console.log("Compléxité de l'ago 2 exp "+aTab.length+" soit "+Math.pow(2, aTab.length))
-console.table(transacCombinaison); 
+console.table(transacCombinaison.table); 
 
 console.log("Une autre méthode plus tactique consiste à trier le tableau de transaction dans l'ordre croissant selon le rapport de rentabilité size/tips .")
 console.log("Puis on parcourt le tableau jusqu'à que la taille du bloc soit atteinte ");
