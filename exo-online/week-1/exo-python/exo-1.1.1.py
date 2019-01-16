13f0508c591c401a6990e2d8c7e584c92d2b739c
Rynthak <mehdi.boutoubat@gmail.com>
2019-01-16 18:29:27 +0100

    Add python exo

--- exo-online/week-1/exo-python/exo-1.1.1.py
+++ exo-online/week-1/exo-python/exo-1.1.1.py
@@ -1,4 +1,27 @@
 from random import randint
-print("Recherche d'un nombre entre 1 et 100 par la machine.")
+print("Recherche d'un nombre entre 1 et 100 par le client.")
 valsearch  = randint(1, 100)
 print("Nombre a rechercher : " + str(valsearch))
+texteMessage    =    "Coucou devine le nombre "
+response="";
+
+while(response!=valsearch and response!=0):
+    response = input(texteMessage)
+    if(response == valsearch):
+        print("Bravo la reponse etait : "+str(valsearch))    
+        break;
+     
+    texteMessage="C'est Plus "
+    
+    if valsearch < response:
+        texteMessage= "C'est Moins "
+    
+    if abs(valsearch-response)>1 and abs(valsearch-response)<=5 :
+        texteMessage= texteMessage+" C'est Tres HOT"
+        
+    if abs(valsearch-response)>5 and abs(valsearch-response)<=10 :
+        texteMessage= texteMessage+" C'est HOT "    
+    
+
+
+print("Fin")    
