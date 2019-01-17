38ab7bb3d54a5ff89f3b9fc5ee9f4640bc974a04
Rynthak <mehdi.boutoubat@gmail.com>
2019-01-17 14:25:59 +0100

    commit temporary exo

---
+++ exo-online/week-1/crypto/exo-2.3.js
@@ -0,0 +1,20 @@
+/*
+ * Entrainement 2.3
+Écrire une fonction qui prend un texte en entrée et un nombre n, retire les espaces et le regroupe les caractères tous les n:
+
+regroupement (“Mes vieilles tantes”, 3) ￫ [“MVIAE”,EILSNS”,”SELTT”]
+ * 
+ */
+
+function regroupement(txt,group){
+	let replaceSpace=txt.replace(/ /g, "");
+	let tabRegroup =[];
+	for(let i =0;i<replaceSpace.lenght;i++){
+		let temp="";
+		
+	}
+	console.log(temp);
+	 
+}
+
+regroupement ("Mes vieilles tantes", 3);
