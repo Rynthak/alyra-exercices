967597fb27b4678854eff2f6a68009fed63e9233
Rynthak <mehdi.boutoubat@gmail.com>
2019-01-17 16:08:00 +0100

    Add courbe exo

--- exo-online/week-1/crypto/exo-4.1.js
+++ exo-online/week-1/crypto/exo-4.1.js
@@ -19,6 +19,10 @@ function CourbeElliptique(a,b){
 		return this.a==Courbe.a && this.b==Courbe.b;
 	}
 	
+	this.toString = function(){
+		return "Paramètres Courbe : a = "+ this.a+ " ; b = "+ this.b;
+	}
+	
 	return this;
 }
 
@@ -30,3 +34,5 @@ var Courbe2 = new CourbeElliptique (1,2);
 
 console.log(Courbe.testPoint(5,5));
 console.log(Courbe.__eq__(Courbe2));
+
+console.log(Courbe.toString());
