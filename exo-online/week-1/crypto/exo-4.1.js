b7ed8900d4a1426991a007555a42046618518991
Rynthak <mehdi.boutoubat@gmail.com>
2019-01-17 16:05:09 +0100

    Add minor fixes

--- exo-online/week-1/crypto/exo-4.1.js
+++ exo-online/week-1/crypto/exo-4.1.js
@@ -5,10 +5,28 @@ function CourbeElliptique(a,b){
 	if(4*Math.pow(a,3)+27*Math.pow(b,2)==0){
 		throw new Error('({'+a+'}, {'+b+'}) n\'est pas une courbe valide');
 	}
+	
+	this.testPoint = function(x,y){
+		if(Math.pow(y,2)==Math.pow(x,3)+this.a*x-b){
+			console.log("Les points {"+x+","+y+") sont sur la courbe !");
+			return true;
+		}
+		console.log("Les points {"+x+","+y+") sont sur la courbe !");
+		return false
+	}
+	
+	this.__eq__ = function (Courbe){
+		return this.a==Courbe.a && this.b==Courbe.b;
+	}
+	
 	return this;
 }
 
  
 var Courbe = new CourbeElliptique (1,2)
+var Courbe2 = new CourbeElliptique (1,2);
 
-console.log(Courbe);
+
+
+console.log(Courbe.testPoint(5,5));
+console.log(Courbe.__eq__(Courbe2));
