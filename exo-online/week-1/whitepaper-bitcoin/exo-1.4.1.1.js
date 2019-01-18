67cb26b67b4bcc4108147f3e2b472e3238bb2bf1
Rynthak <mehdi.boutoubat@gmail.com>
2019-01-18 11:04:38 +0100

    add new exo - work in progress

---
+++ exo-online/week-1/whitepaper-bitcoin/exo-1.4.1.1.js
@@ -0,0 +1,7 @@
+/*Exercice 1.4.1.1
+Écrire un programme qui convertisse un nombre décimal en sa version hexadécimal little endian et hexadécimal big endian sans avoir recours aux fonctions de conversion du langage utilisé
+*/
+
+function conversion(value){
+	
+}
