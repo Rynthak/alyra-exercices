f51f076bb6fca298c0ece0c2dc4a51e38b3e8533
Rynthak <mehdi.boutoubat@gmail.com>
2019-01-18 18:27:08 +0100

    Add exo for VarInt - work in progress. J'ai du mal à comprendre mais je vais réussir !

---
+++ exo-online/week-1/whitepaper-bitcoin/exo1.2.js
@@ -0,0 +1,8 @@
+/*
+ * 
+ * Exercice 1.2
+Compléter la fonction de l’exercice précédent pour convertir un nombre en sa notation variable little endian
+
+Reprendre le programme de conversion de l’exercice précédent pour donner la notation hexadécimale en entier variable d’un nombre donné
+ * 
+ */
