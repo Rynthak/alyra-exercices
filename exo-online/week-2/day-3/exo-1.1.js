46518315c2c5c73a36b20433bc88e5c4637f573b
Rynthak <mehdi.boutoubat@gmail.com>
2019-01-23 12:16:15 +0100

    Add exo 1.1

---
+++ exo-online/week-2/day-3/exo-1.1.js
@@ -0,0 +1,16 @@
+/*
+Exercice 1.1
+Ecrire une fonction qui convertit la cible en difficulté.
+
+calculerDifficulte(1147152896345386682952518188670047452875537662186691235300769792000 -> 23,5
+
+Cible = CiblePrécédente x (bloc[Actuel].date - block[Actuel-2016].date) /(deux semaines en secondes)
+Difficulté actuelle = cible(max)/cible(actuelle) 
+
+avec max ( (2¹⁶ - 1) * 2²⁰⁸ ≈ 2,7 * 10⁶⁷) ;
+
+*/
+
+function calculerDifficulte(cible){
+	
+}
