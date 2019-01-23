ca7d9a88407be6405be068117007133126c78d42
Rynthak <mehdi.boutoubat@gmail.com>
2019-01-23 18:26:47 +0100

    Add fixes

---
+++ exo-online/week-2/day-3/exo-1.3.js
@@ -0,0 +1,17 @@
+/*
+ * 
+ * 
+Exercice 1.3
+Ecrire une fonction qui détermine s’il s’agit d’un bloc pendant lequel la difficulté est réajustée ou non.
+
+blocReajustement(556416) -> true
+
+
+
+Là J'ai pas compris - @TODO
+ * 
+ */
+
+function blocReajustement(){
+	
+}
