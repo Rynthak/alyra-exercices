de1025c41f4e48b9c9e14d340ea3fd76ed9ace61
Rynthak <mehdi.boutoubat@gmail.com>
2019-01-23 18:16:55 +0100

    Add exo

---
+++ exo-online/week-2/day-3/exo-1.2.js
@@ -0,0 +1,10 @@
+/*Exercice 1.2
+Ecrire une fonction qui étant donné le champ Bits d’un bloc en déduit la difficulté*/
+
+
+function calculerDifficulte(HexValue){
+	let max = 2.7 * Math.pow(10,67)	;	 	
+	console.log(max/parseInt(HexValue.substring(4,HexValue.length)+("00".repeat(parseInt(HexValue.substring(2,4),16)-3)),16));
+}
+
+calculerDifficulte('0x1c0ae493');
