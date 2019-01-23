3e3c8a1351940c27c945791ed8de406246aeab08
Rynthak <mehdi.boutoubat@gmail.com>
2019-01-23 18:18:10 +0100

    Minor fixes

--- exo-online/week-2/day-3/exo-1.2.js
+++ exo-online/week-2/day-3/exo-1.2.js
@@ -1,10 +1,9 @@
 /*Exercice 1.2
 Ecrire une fonction qui étant donné le champ Bits d’un bloc en déduit la difficulté*/
-
+/* Bites cf https://bitcoin.stackexchange.com/questions/2924/how-to-calculate-new-bits-value */
 
 function calculerDifficulte(HexValue){
 	let max = 2.7 * Math.pow(10,67)	;	 	
 	console.log(max/parseInt(HexValue.substring(4,HexValue.length)+("00".repeat(parseInt(HexValue.substring(2,4),16)-3)),16));
 }
-
 calculerDifficulte('0x1c0ae493');
