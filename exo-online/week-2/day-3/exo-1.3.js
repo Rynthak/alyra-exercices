32af50a8d8d3f65822d462114200709e406ddd96
Rynthak <mehdi.boutoubat@gmail.com>
2019-01-24 15:59:21 +0100

    Add fixes for missing exo

--- exo-online/week-2/day-3/exo-1.3.js
+++ exo-online/week-2/day-3/exo-1.3.js
@@ -6,12 +6,12 @@ Ecrire une fonction qui détermine s’il s’agit d’un bloc pendant lequel la
 
 blocReajustement(556416) -> true
 
-
-
-Là J'ai pas compris - @TODO
  * 
  */
 
-function blocReajustement(){
+function blocReajustement(value){
 	
+	return value%2016==0;
 }
+let reajust=blocReajustement(556416);
+console.log(reajust);
