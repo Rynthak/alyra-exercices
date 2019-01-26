12534e9013701cb3a8c0473bdcb4047fc7921c9f
Rynthak <mehdi.boutoubat@gmail.com>
2019-01-26 17:08:34 +0100

    add little endian to hexadecimal

--- exo-online/week-2/day-5-defi/assets/script.js
+++ exo-online/week-2/day-5-defi/assets/script.js
@@ -1,14 +1,59 @@
-$(function(){
+$(function() {
+
+	$("#convert_button").click(function() {
+		let type_convert = $("#type_convert").val();
+		let value_to_convert = $("#value_to_convert").val();
+		switch (type_convert) {
+
+		case "1":
+			HexToDecimal(value_to_convert);
+			break;
+		case "2":
+			DecimalToHex(value_to_convert);
+			break;
+		case "3":
+			LittleEndianToHex(value_to_convert);
+			break;
+		case "4":
+			VarIntToDecimal(value_to_convert);
+			break;
+		case "5":
+			BitsFieldToTarget(value_to_convert);
+			break;
+		case "6":
+			TargetToDifficulty(value_to_convert);
+			break;
+		case "7":
+			LittleEndianToHex(value_to_convert);
+			break;
+
+		}
+
+	});
 	
-	$("#convert_button").click(function(){
-		let type_convert=$("1#type_convert").val();
+
 	
-		
-	})
-	
-	
+
 });
 
-function HexToDecimal(value){
+function HexToDecimal(value) {
+	alert(parseInt(value, 16));
+}
+function DecimalToHex(value) {
+	let convert = parseInt(value).toString(16) ;
+	alert(convert);
+}
+function LittleEndianToHex(endian){
+	var r = '0x'+endian.match(/../g).reverse().join('');
+	alert(r);		
+}
+function VarIntToDecimal(value){
 	
+}
+function BitsFieldToTarget(value){
+	
+}
+
+function printResult(value, text) {
+
 }
