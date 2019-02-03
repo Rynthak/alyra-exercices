22b29b1b27a988ea81fbda8a49b032dd76fb43b0
Rynthak <mehdi.boutoubat@gmail.com>
2019-02-03 15:35:37 +0100

    add exo 3.2.2

--- exo-online/week-3/day-3-4/festival.sol
+++ exo-online/week-3/day-3-4/festival.sol
@@ -1,8 +1,8 @@
 pragma solidity ^0.4.25;
 contract CagnotteFestival {
 	mapping(address => uint) organisateurs;
-	
-	
+	string[12] sponsors;
+	uint creneauxLibresSponsor = 12;
 	
 	constructor() public {
 		organisateurs[msg.sender]=100;
@@ -20,4 +20,19 @@ contract CagnotteFestival {
 		return organisateurs[orga]!=0;
 	}
 	/* Fin Exercice 3.2.1 */
+	
+	/* Exercice 3.2.2 - Ajouter des sponsors à une liste */
+	function sponsoriser(string memory nom) payable{
+		require(creneauxLibresSponsor > 0,"Plus de place pour le sponsor");
+		require(msg.value>=30000000000000000000,"Montant inférieurs à 30 ether");
+			
+		sponsors[12-creneauxLibresSponsor]=nom;
+		creneauxLibresSponsor--;		 
+		
+		
+		
+	}
+	
+	 
+	
 }
