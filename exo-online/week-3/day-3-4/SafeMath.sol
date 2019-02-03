fd856f0bd06d61711efb1fd2cb02ef84649d42df
Rynthak <mehdi.boutoubat@gmail.com>
2019-02-03 15:49:22 +0100

    add fixes

--- exo-online/week-3/day-3-4/SafeMath.sol
+++ exo-online/week-3/day-3-4/SafeMath.sol
@@ -1,4 +1,5 @@
-pragma solidity ^0.4.25;
+pragma solidity ^0.5.2;
+
 /**
  * @title SafeMath
  * @dev Unsigned math operations with safety checks that revert on error
@@ -61,43 +62,4 @@ library SafeMath {
         require(b != 0);
         return a % b;
     }
-
-}
-contract CagnotteFestival {
-	mapping(address => uint) organisateurs;
-	using SafeMath for uint256; //
-	string[12] sponsors;
-	uint creneauxLibresSponsor = 12;
-	
-	constructor() public {
-		organisateurs[msg.sender]=100;
-		
-	}
-	
-	/*Exercice 3.2.1 - Donner le statut d'organisateurs */
-	function transfererOrga(address orga, uint parts) public {  
-		require(estOrga(msg.sender),"Operation not allowed");
-		
-		organisateurs[orga]=parts;
-		organisateurs[msg.sender]-=parts;
-	}
-	function estOrga(address orga) public returns (bool){ 
-		return organisateurs[orga]!=0;
-	}
-	/* Fin Exercice 3.2.1 */
-	
-	/* Exercice 3.2.2 - Ajouter des sponsors à une liste */
-	function sponsoriser(string memory nom) payable{
-		require(creneauxLibresSponsor > 0,"Plus de place pour le sponsor");
-		require(msg.value>=30000000000000000000,"Montant inférieurs à 30 ether");
-			
-		sponsors[12-creneauxLibresSponsor]=nom;
-		creneauxLibresSponsor--;		 
-		
-		
-		
-	}
-	
-	 
-	
 }
