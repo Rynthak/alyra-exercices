c4ff111f81009ee30ffa011ad92bfd262ad7a97a
Rynthak <mehdi.boutoubat@gmail.com>
2019-02-03 18:02:09 +0100

    add fixes

--- exo-online/week-3/day-3-4/festival.sol
+++ exo-online/week-3/day-3-4/festival.sol
@@ -69,12 +69,23 @@ library SafeMath {
 }
 contract CagnotteFestival {
 	mapping(address => uint) organisateurs;
+	address [] organisateursList;
+	
 	using SafeMath for uint256; //
 	string[12] sponsors;
 	uint creneauxLibresSponsor = 12;
 	
+	uint256 sponsorRecette;
+	uint256 depenseLimitPerDay=10**18;
+	uint256 dateNow;
+	uint256 actualDepense;
+	uint constant DAY_IN_SECONDS = 86400;
+	
+	uint256 dateLiquidation= 1549297714;
+	
 	constructor() public {
 		organisateurs[msg.sender]=100;
+		organisateursList.push(msg.sender);
 		
 	}
 	
@@ -84,6 +95,7 @@ contract CagnotteFestival {
 		
 		organisateurs[orga]=parts;
 		organisateurs[msg.sender]-=parts;
+		organisateursList.push(orga);
 	}
 	function estOrga(address orga) public returns (bool){ 
 		return organisateurs[orga]!=0;
@@ -94,14 +106,47 @@ contract CagnotteFestival {
 	function sponsoriser(string memory nom) payable{
 		require(creneauxLibresSponsor > 0,"Plus de place pour le sponsor");
 		require(msg.value>=30*10**18,"Montant inférieurs à 30 ether");
-			
+		//Recette des sponsors dépensable
+		sponsorRecette+=msg.value;
+		
 		sponsors[12-creneauxLibresSponsor]=nom;
-		creneauxLibresSponsor--;		 
-		
-		
-		
+		creneauxLibresSponsor--;
 	}
 	
+	/* Exercice 3.2.4 - Gérer le temps dans le contrat du festival */
+
+	function depense(uint256 depenseValue) public{
+		if(uint256(dateNow/DAY_IN_SECONDS)!=uint256(now/DAY_IN_SECONDS) ){
+			dateNow=now;
+			actualDepense=depenseLimitPerDay;
+			if(depenseLimitPerDay>sponsorRecette){
+				actualDepense=sponsorRecette;
+			}
+		}
+		
+		require(int256(actualDepense-depenseValue)>=0,"La dépense dépasse le seuil diponible pour aujourd'hui");
+		require(int256(sponsorRecette-depenseValue)>=0,"Plus assez de recette à dépenser");
+		
+		sponsorRecette-=depenseValue;
+		actualDepense-=depenseValue;
+		
+	}
 	 
+	function liquidationOrganisateur () public{ 
+		require(block.timestamp >= dateLiquidation);
+		address lastOrg;
+		//On liquide les organisateurs
+		for (uint i = 0; i < organisateursList.length; i++) {
+			
+			if(estOrga(organisateursList[i])){
+				lastOrg=organisateursList[i];
+			}
+			
+        	delete(organisateurs[organisateursList[i]]);
+        	delete(organisateursList[i]);
+    	}
+    	//Destruction contract
+		selfdestruct(lastOrg);
+	}
 	
 }
