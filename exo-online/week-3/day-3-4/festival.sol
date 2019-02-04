8a64884de5efd90519fd746e8a2610620f8c896f
Rynthak <mehdi.boutoubat@gmail.com>
2019-02-04 12:18:07 +0100

    add fixes

--- exo-online/week-3/day-3-4/festival.sol
+++ exo-online/week-3/day-3-4/festival.sol
@@ -1,74 +1,11 @@
-pragma solidity ^0.4.25;
-/**
- * @title SafeMath
- * @dev Unsigned math operations with safety checks that revert on error
- */
- /* 
-  * Exercice 3.2.3 - Importer la librairie SafeMath
+pragma solidity ^0.5.3;
 
-  */
-library SafeMath {
-    /**
-     * @dev Multiplies two unsigned integers, reverts on overflow.
-     */
-    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
-        // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
-        // benefit is lost if 'b' is also tested.
-        // See: https://github.com/OpenZeppelin/openzeppelin-solidity/pull/522
-        if (a == 0) {
-            return 0;
-        }
+import "github.com/OpenZeppelin/zeppelin-solidity/contracts/math/SafeMath.sol";
 
-        uint256 c = a * b;
-        require(c / a == b);
-
-        return c;
-    }
-
-    /**
-     * @dev Integer division of two unsigned integers truncating the quotient, reverts on division by zero.
-     */
-    function div(uint256 a, uint256 b) internal pure returns (uint256) {
-        // Solidity only automatically asserts when dividing by 0
-        require(b > 0);
-        uint256 c = a / b;
-        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-
-        return c;
-    }
-
-    /**
-     * @dev Subtracts two unsigned integers, reverts on overflow (i.e. if subtrahend is greater than minuend).
-     */
-    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-        require(b <= a);
-        uint256 c = a - b;
-
-        return c;
-    }
-
-    /**
-     * @dev Adds two unsigned integers, reverts on overflow.
-     */
-    function add(uint256 a, uint256 b) internal pure returns (uint256) {
-        uint256 c = a + b;
-        require(c >= a);
-
-        return c;
-    }
-
-    /**
-     * @dev Divides two unsigned integers and returns the remainder (unsigned integer modulo),
-     * reverts when dividing by zero.
-     */
-    function mod(uint256 a, uint256 b) internal pure returns (uint256) {
-        require(b != 0);
-        return a % b;
-    }
-
-}
 contract CagnotteFestival {
 	mapping(address => uint) organisateurs;
+	 
+	mapping(address => bool) festivaliers;
 	address [] organisateursList;
 	
 	using SafeMath for uint256; //
@@ -89,10 +26,23 @@ contract CagnotteFestival {
 		
 	}
 	
+	function acheterTicket() public payable{
+		requirre(msg.value>( 500 finney),"Place à 0.5 Ethers")
+		festivaliers[msg.sender]=true;
+	}
+	
+	function payer(address payble destinataire,uint montant) public {
+		require(estOrga(msg.sender));
+		require(destinataire !=address(0))
+		require(montant>0);
+		//Adresse payble
+		destinataire.transfer(montant);
+	}
+	
+	
 	/*Exercice 3.2.1 - Donner le statut d'organisateurs */
 	function transfererOrga(address orga, uint parts) public {  
-		require(estOrga(msg.sender),"Operation not allowed");
-		
+		require(organisateurs[msg.sender]>=parts,"Operation not allowed");
 		organisateurs[orga]=parts;
 		organisateurs[msg.sender]-=parts;
 		organisateursList.push(orga);
