df95074472d9f2e84070676788ebd1395bc5be4c
Rynthak <mehdi.boutoubat@gmail.com>
2019-02-04 11:35:10 +0100

    sss

--- exo-online/week-4/day-1/jeton.sol
+++ exo-online/week-4/day-1/jeton.sol
@@ -7,6 +7,8 @@ contract JetonMinimal{
 	mapping (address=>uint256) public comptes;
 	using SafeMath for uint; // 
 	address public owner;
+	uint256 dateEmission=0;
+	event Transfert(uint256,address payeur,address destinataire);
 	
 	constructor(uint256 nombreItial) public{
 		comptes[msg.sender]=nombreItial;
@@ -17,12 +19,14 @@ contract JetonMinimal{
 		require(int(comptes[msg.sender]-value)>0,"Pas assez de jetons");
 		comptes[destinataire]=comptes[destinataire].add(value);
 		comptes[msg.sender]=comptes[msg.sender].sub(value);
-		
+		emit Transfert(valeur,msg.sender,destinataire)
 	}
 	
 	function minting(address destinataire,uint256 value) public{
 		require(msg.sender==owner);
+		require(dateEmission + 1 weeks <= now);
 		comptes[msg.sender]=comptes[msg.sender].add(value);
 		transfert(destinataire,value);
+		dateEmission=now;
 	}
 }
