ab692c2d3deabff4316c77dd10e8760ad14aa900
Rynthak <mehdi.boutoubat@gmail.com>
2019-02-04 10:49:37 +0100

    add fixes for SafeMath

--- exo-online/week-4/day-1/jeton.sol
+++ exo-online/week-4/day-1/jeton.sol
@@ -1,17 +1,28 @@
-pragma solidity ^0.4.25;
+pragma solidity ^0.5.3;
+
+import "github.com/OpenZeppelin/zeppelin-solidity/contracts/math/SafeMath.sol";
 
 contract JetonMinimal{
 	
 	mapping (address=>uint256) public comptes;
+	using SafeMath for uint; // 
+	address public owner;
 	
-	constructor(uint256 nombreItial){
+	constructor(uint256 nombreItial) public{
 		comptes[msg.sender]=nombreItial;
+		owner=msg.sender;
 	}
-	function transfert(address destinataire, uint256 value){
+	function transfert(address destinataire, uint256 value) public{
 	 
 		require(int(comptes[msg.sender]-value)>0,"Pas assez de jetons");
-		comptes[destinataire]+=value;
-		comptes[msg.sender]-=value;
+		comptes[destinataire]=comptes[destinataire].add(value);
+		comptes[msg.sender]=comptes[msg.sender].sub(value);
 		
+	}
+	
+	function minting(address destinataire,uint256 value) public{
+		require(msg.sender==owner);
+		comptes[msg.sender]=comptes[msg.sender].add(value);
+		transfert(destinataire,value);
 	}
 }
