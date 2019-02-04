f984eb7f2302b34916c7f0ec8531ee98d3fc622e
Rynthak <mehdi.boutoubat@gmail.com>
2019-02-04 09:53:18 +0100

    Week-4 day 1

---
+++ exo-online/week-4/day-1/jeton.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.4.25;
+
+contract JetonMinimal{
+	
+	mapping (address=>uint256) public comptes;
+	
+	function constructor(uint256 nombreItial){
+		
+	}
+	function transfert(address desitnataire, uint256 value){
+		
+	}
+}
