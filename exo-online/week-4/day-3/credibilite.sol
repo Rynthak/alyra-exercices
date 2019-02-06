7871836cd84488771d133b868f1a14074ea24c19
Rynthak <mehdi.boutoubat@gmail.com>
2019-02-06 16:22:49 +0100

    Add fixes

--- exo-online/week-4/day-3/credibilite.sol
+++ exo-online/week-4/day-3/credibilite.sol
@@ -11,6 +11,7 @@ contract Credibilite {
 
 
    mapping (address => uint256) public cred;
+   mapping (bytes32 => bool) private devoirsExists;
 
    bytes32[] private devoirs;
     
@@ -21,18 +22,22 @@ contract Credibilite {
    		return keccak256(bytes(url));
    }
    
-   function transfer(address destinataire, uint256 valeur) public{   		
+   function transfer(address destinataire, uint256 valeur) public{
+   		require(destinataire !=address(0));   		
    		require(cred[msg.sender] > valeur);
    		require(cred[destinataire] > 0);    		
    		cred[msg.sender]= cred[msg.sender].sub(valeur);
    		cred[destinataire]  = cred[destinataire].add(valeur);	
    }
    
-   function remettre(bytes32 dev) public returns(uint256){
-   		
-   		uint256 order = devoirs.length;
+   function remettre(bytes32 dev) public returns(uint order){   		
+   		//Vérification anti triche et antiFraude   		
+   		require(devoirsExists[dev]==false,"Petit tricheur ce dévoir existe déjà"); 
+   		require(cred[msg.sender]==0,"Tu as déjà rendu ton devoir");  		
+   		order = devoirs.length;
    		cred[msg.sender]=(order==0)?cred[msg.sender].add(30):cred[msg.sender].add(10);
-   		devoirs.push(dev); 
+   		devoirs.push(dev);
+   		devoirsExists[dev]=true;
    		order=order.add(1);  		
    		return order;
    }
