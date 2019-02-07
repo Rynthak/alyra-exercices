34f6ae89f69eaa500d4370f5d73f147bb3500619
Rynthak <mehdi.boutoubat@gmail.com>
2019-02-07 17:31:52 +0100

    Add new contract With event

---
+++ exo-online/week-4/day-4/contract/credibilite.sol
@@ -0,0 +1,51 @@
+pragma solidity ^0.5.3;
+
+
+import "github.com/OpenZeppelin/openzeppelin-solidity/contracts/math/SafeMath.sol";
+
+
+contract Credibilite {
+
+
+   using SafeMath for uint256;
+
+
+   mapping (address => uint256) public cred;
+   mapping (bytes32 => bool) private devoirsExists;
+
+   bytes32[] private devoirs;
+   event Transfer(address payeur, address destinataire, uint256 montant);
+   event Remise(bytes32 dev, address eleve);
+    
+    
+   /* Exercice 4.1.1 Rédiger un contrat Crédibilité pour remettre des devoirs */
+   
+   function produireHash(string memory url) public pure returns(bytes32){
+   		return keccak256(bytes(url));
+   }
+   
+   function transfer(address destinataire, uint256 valeur) public{
+   		require(destinataire !=address(0));   		
+   		require(cred[msg.sender] > valeur);
+   		require(cred[destinataire] > 0);    		
+   		cred[msg.sender]= cred[msg.sender].sub(valeur);
+   		cred[destinataire]  = cred[destinataire].add(valeur);	
+   		
+   		emit Transfer(msg.sender, destinataire, valeur);
+   }
+   
+   function remettre(bytes32 dev) public returns(uint order){   		
+   		//Vérification anti triche et antiFraude   		
+   		require(devoirsExists[dev]==false,"Petit tricheur ce dévoir existe déjà"); 
+   		require(cred[msg.sender]==0,"Tu as déjà rendu ton devoir");  		
+   		order = devoirs.length;
+   		cred[msg.sender]=(order==0)?cred[msg.sender].add(30):((order==1)?cred[msg.sender].add(20):cred[msg.sender].add(10));
+   		devoirs.push(dev);
+   		devoirsExists[dev]=true;
+   		order=order.add(1);  	
+   		emit Remise(dev,msg.sender);
+   		return order;
+   }
+   /* End exercice 4.1.1 */
+ 
+}
