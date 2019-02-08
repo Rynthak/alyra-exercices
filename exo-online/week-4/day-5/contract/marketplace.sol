edca948e428c05314d0c9cb3736b5e0b55c69c80
Rynthak <mehdi.boutoubat@gmail.com>
2019-02-08 17:31:05 +0100

    Add fixes

--- exo-online/week-4/day-5/contract/marketplace.sol
+++ exo-online/week-4/day-5/contract/marketplace.sol
@@ -9,24 +9,28 @@ contract Marketplace is Ownable {
 	
 	using SafeMath for uint256;
 	
-	mapping (address => uint256) private illustratorReputation;
-	mapping (address => string) private illustratorName;
-	mapping (address => bool) private bannedIllustrator;
-	
-	
+	mapping (address => Illustrator) private illustrators;
+	mapping (address => Entreprise) private entreprises;
 	
 	function inscription(string name) public{
-		
-		require(illustratorReputation[msg.sender]==0,"Vous êtes déjà inscrit");
-		require(bannedIllustrator[msg.sender]==false);
-		
-		illustratorReputation[msg.sender]=1;
-		illustratorName[msg.sender]=name;
+		require(illustrators[msg.sender].illustrator_address == address(0),"Vous êtes déjà inscrit");
+		illustrators[msg.sender]=Illustrator({name:name,reputation:1,illustrator_address:msg.sender});
+	}
+	function inscriptionEntreprises(string name){
+		require(entreprises[msg.sender].entreprise_address== address(0),"Vous êtes déjà inscrit");
+		entreprises[msg.sender]=Entreprise({name:name,entreprise_address:msg.sender});
 	}
 	
 	function banIllustrator(address illustrator) public onlyOwner(){
-		bannedIllustrator[illustrator]=true;
-		illustratorReputation[illustrator]=0;
+		require(illustrators[msg.sender].illustrator_address != address(0),"Vous êtes déjà inscrit");
+		bannedIllustrator[illustrator].status=StatusIllustratorChoice.BAN;
+		bannedIllustrator[illustrator].reputation=0;
+	}
+	
+	function ajouterDemande( uint256 remuneration,uint256 accept_delay,string description,uint256 minimumReput) public payable {
+		
+		
+		entreprises[msg.sender].demandes.push(Demande({remuneration: remuneration, accept_delay: accept_delay,description:description,minimumReput:minimumReput}));
 	}
 	
 }
--- exo-online/week-4/day-5/contract/myStruct.sol
+++ exo-online/week-4/day-5/contract/myStruct.sol
@@ -1,10 +1,25 @@
 library SharedStructs {
-	enum StatusChoice { OUVERTE, ENCOURS, FERMEE }
+	enum StatusChoice { OUVERTE, ENCOURS, FERMEE };
+	enum StatusIllustratorChoice { ACCEPTE, BAN};
     struct Demande {
         uint256 remuneration;
         uint256 accept_delay;
         string description;
-        StatusChoice status;
-        
+        StatusChoice status = 0;
+        uint256 minimumReput = 1;
+        address[] candidats;
+    }
+    
+    struct Entreprise{
+    	 string name;
+    	 address entreprise_address;
+    	 mapping (address => Demande[]) private demandes;
+    }
+    
+    struct Illustrator{
+    	 string name;
+    	 address illustrator_address;
+    	 uint256 reputation ;
+    	 StatusIllustratorChoice status = 0;
     }    
 }
