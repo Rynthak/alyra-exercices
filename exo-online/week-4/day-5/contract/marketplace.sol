3ae8cbe7db12d2b2bf5ca50b7ab3160a28b35918
Rynthak <mehdi.boutoubat@gmail.com>
2019-02-08 15:49:39 +0100

    Add inscription function

--- exo-online/week-4/day-5/contract/marketplace.sol
+++ exo-online/week-4/day-5/contract/marketplace.sol
@@ -7,5 +7,19 @@ import "github.com/OpenZeppelin/openzeppelin-solidity/contracts/ownership/Ownabl
 
 contract Marketplace is Ownable {
 	
+	using SafeMath for uint256;
+	
+	mapping (address => uint256) private illustratorReputation;
+	mapping (address => string) private illustratorName;
+	mapping (address => bool) private bannedIllustrator;
+	
+	function inscription(string name) public{
+		
+		require(illustratorReputation[msg.sender]==0,"Vous êtes déjà inscrit");
+		require(bannedIllustrator[msg.sender]==false);
+		
+		illustratorReputation[msg.sender]=1;
+		illustratorName[msg.sender]=name;
+	}
 	
 }
