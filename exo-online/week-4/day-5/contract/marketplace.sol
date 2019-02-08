bddc73d6d62d02a40b2d78e8ad88931c98072d35
Rynthak <mehdi.boutoubat@gmail.com>
2019-02-08 15:33:40 +0100

    temp commit

---
+++ exo-online/week-4/day-5/contract/marketplace.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.3;
+pragma experimental ABIEncoderV2;
+
+import "github.com/OpenZeppelin/openzeppelin-solidity/contracts/math/SafeMath.sol";
+import "github.com/OpenZeppelin/openzeppelin-solidity/contracts/ownership/Ownable.sol";
+
+
+contract Marketplace is Ownable {
+	
+	
+}
