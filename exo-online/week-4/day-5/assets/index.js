ceaa8feee2a12124f596005c3367e6665c158ffd
Rynthak <mehdi.boutoubat@gmail.com>
2019-02-09 21:12:29 +0100

    Add inscription layout

--- exo-online/week-4/day-5/assets/index.js
+++ exo-online/week-4/day-5/assets/index.js
@@ -1,16 +1,40 @@
  
 $(function() {
     
+	$('.message a').click(function(){
+		   $('[data-rel="form-home"]').animate({height: "toggle", opacity: "toggle"}, "slow");
+	});
+	
+	
     
-	 
+	$('[data-rel="connect"]').click(function(){
+		
+	});
+	
+	$('[data-rel="register"]').click(function(){
+		
+	});
 	
 	
 	
 });
 
- 
+let initContractMarketPlace = async function(){
+	 
+	//Init contract Market Place
+	let contratMarketPlace=new ethers.Contract(contractAddress, abiContract, dapp.provider);
+	
+	
+	
+	
+};
 
-async function createMetaMaskDapp(event) {
+
+
+
+let dapp = null; 
+
+async function createMetaMaskDapp() {
 	
 	 try {
 	   // Demande à MetaMask l'autorisation de se connecter
@@ -18,16 +42,9 @@ async function createMetaMaskDapp(event) {
 	   const address = addresses[0]
 	   // Connection au noeud fourni par l'objet web3
 	   const provider = new ethers.providers.Web3Provider(ethereum);
-	   dapp = { address, provider };
-	   
-	   
-	   $.each(event.data.functionToCall,function(index, value){
-		   value();
-	   })
-	   
-	   
+	   dapp = { address, provider };	   
 	 } catch(err) {
-	   // Gestion des erreurs
+	    
 	   console.error(err);
 	 }
 }
