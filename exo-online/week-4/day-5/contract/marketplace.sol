d900ed2d6727d042d7836d955a3dd67d0e6f49f7
Rynthak <mehdi.boutoubat@gmail.com>
2019-02-09 17:16:10 +0100

    QY

--- exo-online/week-4/day-5/contract/marketplace.sol
+++ exo-online/week-4/day-5/contract/marketplace.sol
@@ -3,8 +3,38 @@ pragma experimental ABIEncoderV2;
 
 import "github.com/OpenZeppelin/openzeppelin-solidity/contracts/math/SafeMath.sol";
 import "github.com/OpenZeppelin/openzeppelin-solidity/contracts/ownership/Ownable.sol";
-import "./myStruct.sol";
-import "./demande.sol";
+
+
+library SharedStructs {
+	
+	enum StatusIllustratorChoice { ACCEPTE, BAN}
+    
+    struct Entreprise{
+    	 string name;
+    	 address entreprise_address;
+    	 Demande[] demandes;
+    }
+    
+    struct Illustrator{
+    	 string name;
+    	 address illustrator_address;
+    	 uint256 reputation ;
+    	 StatusIllustratorChoice status;
+    }    
+}
+
+contract Demande{
+	uint256 remuneration;
+    uint256 accept_delay;
+    string description;
+    
+    enum StatusChoice { OUVERTE, ENCOURS, FERMEE }
+    StatusChoice status ;
+    uint256 minimumReput ;
+    mapping (address => Illustrator) illustrators;
+}
+
+
 
 contract Marketplace is Ownable {
 	
@@ -33,7 +63,7 @@ contract Marketplace is Ownable {
 		//require(msg.value  == (remuneration * 1.2) );
 		//SharedStructs.Demande myDemande= SharedStructs.Demande({remuneration: remuneration, accept_delay: accept_delay,description:description,minimumReput:minimumReput});
 		
-		entreprises[msg.sender].demandes[];
+		entreprises[msg.sender].demandes.push(new Demande(remuneration,accept_delay,description,minimumReput));
 	}
 	
 }
