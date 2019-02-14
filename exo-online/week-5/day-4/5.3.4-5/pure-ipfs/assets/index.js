128711c1d3a13a512361f62bba6ebf1d2ef2b62e
Rynthak <mehdi.boutoubat@gmail.com>
2019-02-15 00:31:04 +0100

    Add fixes

--- exo-online/week-5/day-4/5.3.4-5/pure-ipfs/assets/index.js
+++ exo-online/week-5/day-4/5.3.4-5/pure-ipfs/assets/index.js
@@ -2,11 +2,48 @@
 const node = window.IpfsHttpClient('localhost',5001);
 $(function() {
 	
-	 
+	$("#image_file").on('change',function(e){
+		var file = e.target.files[0];
+		  if (!file) {
+		    return;
+		  }
+		  var reader = new FileReader();
+		  reader.onloadend = function(e) {
+			  
+			  
+		        node.add(node.types.Buffer.from(btoa(reader.result),"base64"), (err, result) => { // Upload buffer to IPFS
+		          if(err) {
+		            console.error(err)
+		            return
+		          }
+		          let url = `https://ipfs.io/ipfs/${result[0].hash}`
+		          console.log(`Url --> ${url}`)
+		          let hash = result[0].hash;
+		          notify("L'image a bien été uplaoder <br>"+url,"Info",'notice');
+		          loadImageByHash(hash);
+		         
+		        })
+		    
+		    
+		    
+		  };
+		  reader.readAsBinaryString(file);
+		   
+	});
 	 
  });
  
- 
+var loadImageByHash = function(hash){
+	 
+    node.cat(hash, function (err, file) {
+	    if (err) {
+	        throw err
+	    }    
+	    $("#output").attr('src', "data:image/blob;base64," + file.toString("base64"));
+    });
+} 
+
+
  function notify(message,title="Erreur",type="error"){
 	$.growl({ title: title, message: message,style:type});
 }
