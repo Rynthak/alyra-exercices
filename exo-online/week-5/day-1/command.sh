9dd997f513f07fc6aa8f717dfc348e4f79fab98f
Rynthak <mehdi.boutoubat@gmail.com>
2019-02-14 18:03:29 +0100

    Add minor fixes

--- exo-online/week-5/day-1/command.sh
+++ exo-online/week-5/day-1/command.sh
@@ -26,3 +26,33 @@ geth  --verbosity 3 --datadir /Users/rynthak/privateChain/alice --syncmode 'full
 NODE_ENV=production INSTANCE_NAME=alice CONTACT_DETAILS=Mehdi RPC_HOST=localhost RPC_PORT=30312 WS_SERVER=http://localhost:3000 WS_SECRET=nirifa94 LISTENING_PORT=30311  node app.js
 NODE_ENV=production INSTANCE_NAME=BOB CONTACT_DETAILS=Mehdi RPC_HOST=localhost RPC_PORT=30314 WS_SERVER=http://localhost:3000 WS_SECRET=nirifa94 LISTENING_PORT=30313  node app.js
 NODE_ENV=production INSTANCE_NAME=Claudia CONTACT_DETAILS=Mehdi RPC_HOST=localhost RPC_PORT=30316 WS_SERVER=http://localhost:3000 WS_SECRET=nirifa94 LISTENING_PORT=30315  node app.js
+
+
+
+#MIST
+mist --rpc  http://52.143.167.179:8545
+
+
+#Noeud Görli
+geth --datadir /home/rynthak/goerli/mehdi  --port 30311 --syncmode 'full' --rpc  --rpcaddr 0.0.0.0 --rpccorsdomain “*”   --rpcport 8546 --rpcapi 'db,eth,net,web3,personal,web3' --bootnodes 'enode://c1f8b7c2ac4453271fa07d8e9ecf9a2e8285aa0bd0c07df0131f47153306b0736fd3db8924e7a9bf0bed6b1d8d4f87362a71b033dc7c64547728d953e43e59b2@52.64.155.147:30303' --networkid 5 
+
+geth attach http://52.143.167.179:8546
+
+geth  --datadir /home/rynthak/goerli/mehdi --nat "any" --syncmode 'fast' --rpc  --rpcaddr 0.0.0.0 --rpccorsdomain “*”   --rpcport 8546 --rpcapi 'db,eth,net,web3,personal,web3' --bootnodes 'enode://176b9417f511d05b6b2cf3e34b756cf0a7096b3094572a8f6ef4cdcb9d1f9d00683bf0f83347eebdf3b81c3521c2332086d9592802230bf528eaf606a1d9677b@13.93.54.137:30303' --networkid 5 
+
+geth   --syncmode 'fast' --rpc  --rpcaddr 0.0.0.0    --rpcport 8546 --rpcapi 'db,eth,net,web3,personal,web3' --bootnodes 'enode://c1f8b7c2ac4453271fa07d8e9ecf9a2e8285aa0bd0c07df0131f47153306b0736fd3db8924e7a9bf0bed6b1d8d4f87362a71b033dc7c64547728d953e43e59b2@52.64.155.147:30303' --networkid 5 
+
+
+geth  --datadir /home/rynthak/goerli/ --syncmode 'fast' --rpc  --rpcaddr 0.0.0.0     --rpcport 8546 --rpcapi 'db,eth,net,web3,personal,web3' --bootnodes 'enode://46add44b9f13965f7b9875ac6b85f016f341012d84f975377573800a863526f4da19ae2c620ec73d11591fa9510e992ecc03ad0751f53cc02f7c7ed6d55c7291@94.237.54.114:30313' --networkid 5 
+
+wget https://raw.githubusercontent.com/goerli/testnet/master/geth/goerli.genesis
+
+geth  --datadir data --syncmode 'fast' --rpc  --rpcaddr 0.0.0.0     --rpcport 8546 --rpcapi 'db,eth,net,web3,personal,web3' --bootnodes 'enode://46add44b9f13965f7b9875ac6b85f016f341012d84f975377573800a863526f4da19ae2c620ec73d11591fa9510e992ecc03ad0751f53cc02f7c7ed6d55c7291@94.237.54.114:30313' --networkid 5 
+
+wget https://raw.githubusercontent.com/goerli/testnet/master/geth/goerli.genesis
+geth -datadir data init goerli.genesis
+geth  --datadir data --syncmode 'full' --rpc  --rpcaddr 0.0.0.0     --rpcport 8546 --rpcapi 'db,eth,net,web3,personal,web3' --bootnodes 'enode://176b9417f511d05b6b2cf3e34b756cf0a7096b3094572a8f6ef4cdcb9d1f9d00683bf0f83347eebdf3b81c3521c2332086d9592802230bf528eaf606a1d9677b@13.93.54.137:30303' --networkid 5 
+
+geth attach http://52.143.167.179:8546
+eth.getBalance('0xba5efca7724d335c139ac4bfb5a3498f2c6c2482')
+
--- exo-online/week-5/day-2/assets/index.js
+++ exo-online/week-5/day-2/assets/index.js
@@ -1,15 +1,41 @@
 const node = new Ipfs({repo:'ipfs-'+Math.random()});
+//const ipfs = window.IpfsApi('localhost',5001);
 $(function() {
 	 
 	
-	 
+	$("#image_file").on('change',function(e){
+		var file = e.target.files[0];
+		  if (!file) {
+		    return;
+		  }
+		  var reader = new FileReader();
+		  reader.onloadend = function(e) {
+			  
+			  const buf = buffer.Buffer(reader.result) // Convert data into buffer
+		        node.add(buf, (err, result) => { // Upload buffer to IPFS
+		          if(err) {
+		            console.error(err)
+		            return
+		          }
+		          let url = `https://ipfs.io/ipfs/${result[0].hash}`
+		          console.log(`Url --> ${url}`)
+		          
+		          document.getElementById("output").src = url
+		        })
+		    
+		    
+		    
+		  };
+		  reader.readAsArrayBuffer(file);
+		   
+	})
 	 
         
 	 node.on('ready', () => {
 		 notify("Le noeud est prêt","Info",'notice');
 	 })
 	 
-	 //const ipfs = window.IpfsApi('localhost',5001);
+	 
 	 
 	 $(document).on('submit','[data-rel="add-charipfs"]',function(e){
 		 e.preventDefault();
@@ -38,6 +64,20 @@ $(function() {
  });
  //Ajout du texte
  var addText =  function(txt){
+	 node.add(new node.types.Buffer.from(txt.toString()),(err, result) =>{
+		 if(err){
+			 console.error('erreur  - IPFS add',err,result);
+			 
+			 return false;
+		 }
+		 notify("La chaine "+txt+' a été ajouté',"Info",'notice');
+		 
+		 console.log(result[0].hash);
+		 notify("Voici le hash :"+result[0].hash+'',"Info",'notice');
+	 });
+ }
+ 
+ var addImage =  function(data){
 	 node.add(new node.types.Buffer.from(txt.toString()),(err, result) =>{
 		 if(err){
 			 console.error('erreur  - IPFS add',err,result);
