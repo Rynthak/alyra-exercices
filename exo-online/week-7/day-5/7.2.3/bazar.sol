80a2578e7bfc170100e7245eeccbf7b907c05aef
Rynthak <mehdi.boutoubat@gmail.com>
2019-03-03 16:42:51 +0100

    Init BazarV3

--- exo-online/week-7/day-5/7.2.3/bazar.sol
+++ exo-online/week-7/day-5/7.2.3/bazar.sol
@@ -1,8 +1,14 @@
 pragma solidity ^0.5.3;
-
+pragma experimental ABIEncoderV2;
 import "github.com/Rynthak/alyra-exercices/exo-online/week-7/day-5/7.2.2/bazar.sol";
  
 contract bazarV3 is bazarV2{
 	
-	 
+	 enum BidChoice { CLASSIC, DUTCH}
+	 struct MultiBids {
+	     Enchere bid;
+	     BidChoice types;
+	}
+	mapping (uint256 => MultiBids) public allbids;
+	
 }
