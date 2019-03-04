72f7a5c635fa8bec91545ddc6414a99f3250de02
Rynthak <mehdi.boutoubat@gmail.com>
2019-03-04 12:27:47 +0100

    Add fixes

--- exo-online/week-8/day-1/contracts/SimpleContract.sol
+++ exo-online/week-8/day-1/contracts/SimpleContract.sol
@@ -6,13 +6,10 @@ contract SimpleContract is Ownable{
 	string public name;
 	event NameChanged(string name);
 	
-	
 	constructor(string memory _name) public {
 		name = _name;
-		
     }
     
-   
 	function setName(string memory _name)  public onlyOwner(){
 		name = _name;
 		emit NameChanged(_name);
