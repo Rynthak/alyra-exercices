dcbe5469106992b977c892d30e4a7153940d3805
Rynthak <mehdi.boutoubat@gmail.com>
2019-03-09 17:41:15 +0100

    Add fixes

--- exo-online/week-7/day-5/defi-4/contracts/MonstersContract.sol
+++ exo-online/week-7/day-5/defi-4/contracts/MonstersContract.sol
@@ -7,13 +7,21 @@ import "github.com/OpenZeppelin/openzeppelin-solidity/contracts/token/ERC721/ERC
 contract MonstersContract is ERC721 {
 	
 	
-	struct Monster {
-		uint64 birthTime;        
-        uint256[] battles;         
-    }
+   struct Monster {
+		uint64 birthTime;
+		uint256[] battles;
+   }
+   
+   struct Battle{
+   		uint256 dateBattle;   		
+   		uint256 whoWin;
+   		uint256 opponent1;
+   		uint256 opponent2;
+   }
 	
    Monster[] public monsters;
-	 
+   Battle[] public battles;
+   
    mapping (uint256 => address) public monsterIndexToOwner;
 
    
@@ -30,14 +38,29 @@ contract MonstersContract is ERC721 {
 	function _createMonster() internal  returns (uint){
 		Monster memory _monster = Monster({            
             birthTime: uint64(now),
-            battles: new uint256[](0)
+           	battles: new uint256[](0)
         });        
         uint256 newMonsterId = monsters.push(_monster) - 1;       
         return newMonsterId;
     }
     
-    function batteMonsters(uint256 firstMonster,uint256 secondMonster){
+    function battleMonsters(uint256 firstMonster,uint256 secondMonster) public{
+    	require(_exists(firstMonster) && _exists(secondMonster));
+    	require(ownerOf(firstMonster) == msg.sender);
     	
+    	uint256 random=uint256(blockhash(block.number-1));
+    	uint256 resultBattle = (random%monsters[firstMonster].birthTime < random%monsters[secondMonster].birthTime)?secondMonster:firstMonster;
+    	
+    	Battle memory _battle = Battle({
+    		dateBattle : uint64(now),
+    		whoWin : resultBattle,
+    		opponent1  : firstMonster,
+    		opponent2  : secondMonster
+    	});
+    	uint256 newBattleId = battles.push(_battle) - 1; 
+    	
+    	monsters[firstMonster].battles.push(newBattleId);
+    	monsters[secondMonster].battles.push(newBattleId);
     }
     
 	
