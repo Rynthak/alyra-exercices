9d04995c455ce0b2547796199ff8441dae7e3b03
Rynthak <mehdi.boutoubat@gmail.com>
2019-03-09 17:10:51 +0100

    Add fix

--- exo-online/week-7/day-5/defi-4/contracts/MonstersContract.sol
+++ exo-online/week-7/day-5/defi-4/contracts/MonstersContract.sol
@@ -8,14 +8,11 @@ contract MonstersContract is ERC721 {
 	
 	
 	struct Monster {
-        
-        uint256 genes;
-        uint64 birthTime;        
-        uint64 cooldownEndTime;         
-        uint16 generation;
+		uint64 birthTime;        
+        uint256[] battles;         
     }
 	
-	
+   Monster[] public monsters;
 	 
    mapping (uint256 => address) public monsterIndexToOwner;
 
@@ -24,31 +21,19 @@ contract MonstersContract is ERC721 {
 
    // Adresse vers laquelle le transfert à été approuvé
    mapping (uint256 => address) public monsterIndexToApproved;
-
-   
-	
-   uint32[14] public cooldowns = [
-        uint32(1 minutes),
-        uint32(2 minutes),
-        uint32(5 minutes),
-        uint32(10 minutes),
-        uint32(30 minutes),
-        uint32(1 hours),
-        uint32(2 hours),
-        uint32(4 hours),
-        uint32(8 hours),
-        uint32(16 hours),
-        uint32(1 days),
-        uint32(2 days),
-        uint32(4 days),
-        uint32(7 days)
-    ];
-	uint256 public secondsPerBlock = 15;
-	
+ 
 	function buymonster() public payable{
-		uint256 tokenId = uint256((blockhash(block.number-1)) )%3000;		
+		uint256 tokenId = _createMonster();		
 		_mint(msg.sender,tokenId);
 	}
 	
+	function _createMonster() internal  returns (uint){
+		Monster memory _monster = Monster({            
+            birthTime: uint64(now),
+            battles: new uint256[](0)
+        });        
+        uint256 newMonsterId = monsters.push(_monster) - 1;       
+        return newMonsterId;
+	}
 	
 }
