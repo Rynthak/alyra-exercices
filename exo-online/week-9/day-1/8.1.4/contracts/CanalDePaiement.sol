299fdae4913c4e6208c661e5f933bc623d7b12dd
Rynthak <mehdi.boutoubat@gmail.com>
2019-03-12 12:09:43 +0100

    Add fixes

---
+++ exo-online/week-9/day-1/8.1.4/contracts/CanalDePaiement.sol
@@ -0,0 +1,14 @@
+
+pragma solidity ^0.5.3;
+
+
+import "github.com/Rynthak/alyra-exercices/exo-online/week-9/day-1/8.1.3/contracts/CanalDePaiement.sol";
+
+contract CanalDePaiementV4 is CanalDePaiementV3 {
+	
+	 
+	 function fermeture(uint _nonce, uint _equilibreA, uint _equilibreB bytes _signature)
+	 	 
+	 }
+	 
+}
