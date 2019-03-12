f7078ef1bb8e64eded01853293cb1556c2771ba1
Rynthak <mehdi.boutoubat@gmail.com>
2019-03-12 12:17:13 +0100

    Add fixes

--- exo-online/week-9/day-1/8.1.3/contracts/CanalDePaiement.sol
+++ exo-online/week-9/day-1/8.1.3/contracts/CanalDePaiement.sol
@@ -7,7 +7,7 @@ import "github.com/Rynthak/alyra-exercices/exo-online/week-9/day-1/8.1.2/contrac
 contract CanalDePaiementV3 is CanalDePaiementV2 {
 	
 	 
-	 function makeMessage(uint nonce, uint equilibre) public pure return (bytes32){
+	 function makeMessage(uint nonce, uint equilibre) public pure returns (bytes32){
 	 	return  keccak256(abi.encodePacker(nonce,equilibre));
 	 }
 	 
--- exo-online/week-9/day-1/8.1.4/contracts/CanalDePaiement.sol
+++ exo-online/week-9/day-1/8.1.4/contracts/CanalDePaiement.sol
@@ -3,11 +3,15 @@ pragma solidity ^0.5.3;
 
 
 import "github.com/Rynthak/alyra-exercices/exo-online/week-9/day-1/8.1.3/contracts/CanalDePaiement.sol";
+import "github.com/OpenZeppelin/openzeppelin-solidity/contracts/cryptography/ECDSA.sol";
+
 
 contract CanalDePaiementV4 is CanalDePaiementV3 {
 	
 	 
-	 function fermeture(uint _nonce, uint _equilibreA, uint _equilibreB bytes _signature)
+	 function fermeture(uint _nonce, uint _equilibreA, uint _equilibreB, bytes _signature) public {
+	 	 
+	 	 
 	 	 
 	 }
 	 
