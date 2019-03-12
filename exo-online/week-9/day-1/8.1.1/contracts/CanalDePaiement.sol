627e95fb0be35543a3e9709b02d90f4e3b3a4ea3
Rynthak <mehdi.boutoubat@gmail.com>
2019-03-12 11:48:23 +0100

    Init canal payment

---
+++ exo-online/week-9/day-1/8.1.1/contracts/CanalDePaiement.sol
@@ -0,0 +1,25 @@
+
+pragma solidity ^0.5.3;
+
+
+contract CanalDePaiement {
+	
+	enum EtatCanal { VIDE, ACTIF, ENCOURSFERMETURE, FERME}
+	
+	address partieA;
+	address partieB;
+	uint montant;
+	EtatCanal etat;
+	uint blocFermeture; 
+	uint dernierNonce;
+	uint equilibreA;
+	uint equilibreB;
+	
+	
+	constructor(address _partieA,address _partieB , uint _montant) public {
+		partieA	=_partieA;
+		partieB	=_partieB;
+		montant	= _montant;
+    }
+	
+}
