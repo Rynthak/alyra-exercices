1f752a477f45f26097fb1ac91d2eb01a092d75f5
Rynthak <mehdi.boutoubat@gmail.com>
2019-03-12 12:18:07 +0100

    Add fixes

--- exo-online/week-9/day-1/8.1.2/contracts/CanalDePaiement.sol
+++ exo-online/week-9/day-1/8.1.2/contracts/CanalDePaiement.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.5.3;
 
 
-import "github.com/Rynthak/alyra-exercices/exo-online/week-9/day-1/8.1.1/contracts/CanalDePaiement.sol"
+import "github.com/Rynthak/alyra-exercices/exo-online/week-9/day-1/8.1.1/contracts/CanalDePaiement.sol";
 
 contract CanalDePaiementV2 is CanalDePaiement {
 	
